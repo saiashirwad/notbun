fe7db864de1db287830389fac6f3c5b1ce42d81d
Sai Ashirwad <saiashirwadr@gmail.com>
2024-04-29 05:26:15 +0530

    more changes

--- index.ts
+++ index.ts
@@ -1,9 +1,40 @@
-import { char, choice, many } from "./src/Parser";
+import {
+	char,
+	flatMap,
+	many,
+	andThen,
+	spaces,
+	map,
+	betweenChars,
+	alphabet,
+	skipUntil,
+} from "./src/Parser";
 
 const aParser = char("a");
 const bParser = char("b");
 const manyAParser = many(aParser);
+const word = many(alphabet);
 
-const result = choice([manyAParser, aParser, bParser]).run("aacsd");
+function lol() {
+	let rest = "abb";
+	// const parser = between(
+	// 	["(", ")"],
+	// 	andThen(
+	// 		andThen(andThen(manyAParser, bParser), char("c")),
+	// 		map(spaces(char("a")), (s) => {
+	// 			return s;
+	// 		}),
+	// 	),
+	// );
+	// const parser_ = map(parser, (a) => {
+	// 	return "lol";
+	// });
+	const parser = map(andThen(skipUntil(word), word), ([_, s]) => s.join(""));
+	const result = parser("hithere");
+	console.log(result);
+}
+lol();
 
-console.log(result);
+// const result = choice([manyAParser, aParser, bParser]).run("aacsd");
+//
+// console.log(result);
--- src/Parser.ts
+++ src/Parser.ts
@@ -1,168 +1,178 @@
 import { Either } from "effect";
-import type { TypeLambda } from "effect/HKT";
-import * as Gen from "effect/Utils";
+import { isRight } from "effect/Either";
 
-export namespace Parser {
-	type Input = string;
+type Input = string;
 
-	export type Parser<T> = {
-		run: (input: Input) => Either.Either<[T, Input], string>;
-	};
+export type ParserResult<T> = Either.Either<[T, Input], string>;
+export type Parser<T> = (input: Input) => ParserResult<T>;
 
-	interface ParserTypeLambda extends TypeLambda {
-		readonly type: Parser<"something">;
-	}
-
-	const adapter = Gen.adapter<ParserTypeLambda>();
-
-	// export const gen: Gen.Gen<ParserTypeLambda, Gen.Adapter<ParserTypeLambda>> = (
-	// 	f,
-	// ) => {
-	// 	const iterator = f(adapter);
-	// 	let state: IteratorYieldResult<any> | IteratorReturnResult<any> =
-	// 		iterator.next();
-	// 	if (state.done) {
-	// 	} else {
-	// 		let current = state.value;
-	// 		if (Gen.isGenKind(current)) {
-	// 			current = current.value;
-	// 		} else {
-	// 			current = Gen.yieldWrapGet(current);
-	// 		}
-	// 	}
-	// };
-
-	export const map = <A, B>(parser: Parser<A>, f: (a: A) => B): Parser<B> => ({
-		run: (input) =>
-			Either.match(parser.run(input), {
-				onLeft: (e) => Either.left(e),
-				onRight: ([a, rest]) => Either.right([f(a), rest]),
-			}),
-	});
-
-	export const flatMap = <A, B>(
-		parser: Parser<A>,
-		f: (a: A) => Parser<B>,
-	): Parser<B> => ({
-		run: (input) =>
-			Either.match(parser.run(input), {
-				onRight: ([a, rest]) => f(a).run(rest),
-				onLeft: (e) => Either.left(e),
-			}),
-	});
-
-	export const getRest = <T>(
-		parserOutput: Either.Either<[T, Input], string>,
-	): string => {
-		return Either.match(parserOutput, {
-			onLeft: () => "",
-			onRight: ([_, rest]) => rest,
+export const map =
+	<A, B>(parser: Parser<A>, f: (a: A) => B): Parser<B> =>
+	(input) =>
+		Either.match(parser(input), {
+			onRight: ([a, rest]) => Either.right([f(a), rest]),
+			onLeft: (e) => Either.left(e),
 		});
-	};
 
-	// export const getResult = <T>(
-	// 	parserOutput: Either.Either<[T, Input], string>,
-	// ): Option.Option<T> => {
-	// 	return Either.match(parserOutput, {
-	// 		onLeft: () => Option.none(),
-	// 		onRight: ([result, _]) => Option.some(result),
-	// 	});
-	// };
-}
+export const flatMap =
+	<A, B>(parser: Parser<A>, f: (a: A) => Parser<B>): Parser<B> =>
+	(input) =>
+		Either.match(parser(input), {
+			onRight: ([a, rest]) => f(a)(rest),
+			onLeft: (e) => Either.left(e),
+		});
 
-export const char = (ch: string): Parser.Parser<string> => ({
-	run: (input) => {
+export const andThen =
+	<A, B>(parserA: Parser<A>, parserB: Parser<B>): Parser<readonly [A, B]> =>
+	(input) =>
+		Either.match(parserA(input), {
+			onRight: ([a, rest]) =>
+				Either.match(parserB(rest), {
+					onLeft: (e) => Either.left(e),
+					onRight: ([b, rest]) => Either.right([[a, b], rest]),
+				}),
+			onLeft: (e) => Either.left(e),
+		});
+
+export const getRest = <T>(
+	parserOutput: Either.Either<[T, Input], string>,
+): string => {
+	return Either.match(parserOutput, {
+		onLeft: () => "",
+		onRight: ([_, rest]) => rest,
+	});
+};
+
+export const char =
+	(ch: string): Parser<string> =>
+	(input) => {
 		if (input.startsWith(ch)) {
 			return Either.right([ch, input.slice(1)]);
 		}
 		return Either.left(`${ch} not matched!`);
-	},
-});
+	};
 
-export const between =
-	(start: string) =>
-	(end: string): Parser.Parser<undefined> => ({
-		run: (input) => {
-			if (!input.startsWith(start)) {
-				return Either.left(`Not surrounded by ${start} ${end}`);
-			}
-			if (input.at(-1) !== end) {
-				return Either.left(`Expected matching ${end} for ${start}`);
-			}
-			return Either.right([undefined, input.slice(1, -1)]);
-		},
-	});
+export const alphabet: Parser<string> = (input) => {
+	const [first, ...rest] = input;
+	if (/^[a-zA-Z]$/.test(first)) {
+		return Either.right([first, input.slice(1)]);
+	}
+	return Either.left("Not alphabet");
+};
+
+export const betweenChars =
+	<T>([start, end]: [string, string], parser: Parser<T>): Parser<T> =>
+	(input) => {
+		return map(
+			andThen((input) => {
+				if (!input.startsWith(start)) {
+					return Either.left(`Not surrounded by ${start} ${end}`);
+				}
+				if (input.at(-1) !== end) {
+					return Either.left(`Expected matching ${end} for ${start}`);
+				}
+				return Either.right([undefined, input.slice(1, -1)]);
+			}, parser),
+			([_, t]) => t,
+		)(input);
+	};
 
 const many_ =
 	<T>(count: number) =>
-	(parser: Parser.Parser<T>): Parser.Parser<Array<T>> => ({
-		run: (input) => {
-			const acc: T[] = [];
-			let rest = input;
-			while (true) {
-				const result = parser.run(rest);
-				if (Either.isLeft(result)) {
-					if (acc.length >= count) {
-						return Either.right([acc, rest]);
-					} else {
-						return Either.left(
-							`many_(${count}) expected, but only ${acc.length} found`,
-						);
-					}
+	(parser: Parser<T>): Parser<Array<T>> =>
+	(input) => {
+		const acc: T[] = [];
+		let rest = input;
+		while (true) {
+			const result = parser(rest);
+			if (Either.isLeft(result)) {
+				if (acc.length >= count) {
+					return Either.right([acc, rest]);
+				} else {
+					return Either.left(
+						`many_(${count}) expected, but only ${acc.length} found`,
+					);
 				}
-				rest = Parser.getRest(result);
-				acc.push(result.right[0]);
 			}
-			return Either.right([acc, rest]);
-		},
-	});
+			rest = getRest(result);
+			acc.push(result.right[0]);
+		}
+		return Either.right([acc, rest]);
+	};
 
 const skipMany_ =
 	<T>(count: number) =>
-	(parser: Parser.Parser<T>): Parser.Parser<undefined> => ({
-		run: (input) => {
-			const acc: T[] = [];
-			let rest = input;
-			while (true) {
-				const result = parser.run(rest);
-				if (Either.isLeft(result)) {
-					if (acc.length >= count) {
-						return Either.right([undefined, rest]);
-					} else {
-						return Either.left(
-							`skipMany_(${count}) expected, but only ${acc.length} found`,
-						);
-					}
+	(parser: Parser<T>): Parser<undefined> =>
+	(input) => {
+		const acc: T[] = [];
+		let rest = input;
+		while (true) {
+			const result = parser(rest);
+			if (Either.isLeft(result)) {
+				if (acc.length >= count) {
+					return Either.right([undefined, rest]);
+				} else {
+					return Either.left(
+						`skipMany_(${count}) expected, but only ${acc.length} found`,
+					);
 				}
-				rest = Parser.getRest(result);
-				acc.push(result.right[0]);
 			}
-			return Either.right([undefined, rest]);
-		},
-	});
+			rest = getRest(result);
+			acc.push(result.right[0]);
+		}
+		return Either.right([undefined, rest]);
+	};
 
-export const many = <T>(parser: Parser.Parser<T>) => many_<T>(0)(parser);
-export const many1 = <T>(parser: Parser.Parser<T>) => many_<T>(1)(parser);
-export const manyN = <T>(parser: Parser.Parser<T>, n: number) =>
-	many_<T>(n)(parser);
+export const skipUntil =
+	<T>(parser: Parser<T>): Parser<undefined> =>
+	(input) => {
+		let rest = input;
+		while (true) {
+			if (rest.length === 0) {
+				return Either.left("oops");
+			}
+			const result = parser(input);
+			if (Either.isRight(result)) {
+				return Either.right([undefined, rest]);
+			}
+			rest = rest.slice(1);
+		}
+		return Either.left("wtf");
+	};
 
-export const skipMany = <T>(parser: Parser.Parser<T>) =>
-	skipMany_<T>(0)(parser);
-export const skipMany1 = <T>(parser: Parser.Parser<T>) =>
-	skipMany_<T>(1)(parser);
-export const skipManyN = <T>(parser: Parser.Parser<T>, n: number) =>
+// export const between =
+// 	<T>(
+// 		[start, end]: [Parser<unknown>, Parser<unknown>],
+// 		parser: Parser<T>,
+// 	): Parser<T> =>
+// 	(input) => {};
+
+export const many = <T>(parser: Parser<T>) => many_<T>(0)(parser);
+export const many1 = <T>(parser: Parser<T>) => many_<T>(1)(parser);
+export const manyN = <T>(parser: Parser<T>, n: number) => many_<T>(n)(parser);
+
+export const skipMany = <T>(parser: Parser<T>) => skipMany_<T>(0)(parser);
+export const skipMany1 = <T>(parser: Parser<T>) => skipMany_<T>(1)(parser);
+export const skipManyN = <T>(parser: Parser<T>, n: number) =>
 	skipMany_<T>(n)(parser);
 
-export const choice = (
-	parsers: Array<Parser.Parser<unknown>>,
-): Parser.Parser<unknown> => ({
-	run: (input) => {
+export const newLine = char("\n");
+
+export const spaces = <T>(parser: Parser<T>) =>
+	map(andThen(skipMany(char(" ")), parser), ([_, t]) => t);
+
+export const choice =
+	(parsers: Array<Parser<unknown>>): Parser<unknown> =>
+	(input) => {
 		for (const parser of parsers) {
-			const result = parser.run(input);
+			const result = parser(input);
 			if (Either.isRight(result)) {
 				return result;
 			}
 		}
 		return Either.left("None of the choices could be satisfied");
-	},
-});
+	};
+
+export const Combinators = {
+	"<|>": choice,
+};
